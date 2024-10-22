b895d8f189febf2c4d696e19276e17893776270b
Marta <marta.tafuri@niva.it>
2024-10-22 16:55:53 +0200

    css login

--- resources/views/template_2/commons/head.blade.php
+++ resources/views/template_2/commons/head.blade.php
@@ -3,5 +3,5 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>INDUSTRIA 4.0</title>
-    <link rel="stylesheet" href="{{ asset('build/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ mix('build/css/all.css') }}">
 </head>
