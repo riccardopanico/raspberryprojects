a62b6627768db4b3811eb2a7339ca8ec26419779
Marta <marta.tafuri@niva.it>
2024-10-29 15:45:37 +0100

    window scroll

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -3,15 +3,23 @@ $.ajaxSetup({
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     },
     statusCode: {
-        301: function(responseObject, textStatus, errorThrown) {
+        301: function (responseObject, textStatus, errorThrown) {
             console.log(responseObject, textStatus, errorThrown);
         },
-        302: function(responseObject, textStatus, errorThrown) {
+        302: function (responseObject, textStatus, errorThrown) {
             window.location.href = responseObject.responseJSON.url_redirect;
         },
-        419: function(responseObject, textStatus, errorThrown) {
+        419: function (responseObject, textStatus, errorThrown) {
             // window.location.href = "logout"
         }
     },
     timeout: 0
+});
+
+$(document).ready(function () {
+    setInterval(function () {
+        if (!$('#KioskBoard-VirtualKeyboard').length) {
+            window.scrollTo(0, 0);
+        }
+    }, 1000);
 });
