b8e5f891367442b1ac1fe54fc1090a8bdda324c9
Riccardo Panico <rikkardo.panico@gmail.com>
2024-10-30 12:56:33 +0100

    fix modal e boot

--- resources/views/template_1/commons/modal.blade.php
+++ resources/views/template_1/commons/modal.blade.php
@@ -13,3 +13,120 @@
         </div>
     </div>
 </div>
+<script>
+
+    function openModal(action) {
+            var $modal = $('#modal-xl').clone();
+            $modal.removeAttr('id');
+
+            const modalSettingsDefault = {
+                header: '',
+                headerVisible: true,
+                headerClass: 'modal-header',
+                dialogClass: 'modal-dialog',
+                body: '',
+                bodyVisible: true,
+                bodyClass: 'modal-body',
+                footer: '',
+                footerVisible: true,
+                footerClass: '',
+                title: '',
+                positionClass: 'modal-xl modal-dialog-centered',
+                forzaFocus: false,
+                action: () => {},
+                onShow: () => {},
+                onClose: () => {},
+            };
+
+            const modalSettings = {
+                'filato': {
+                    title: '<strong>Richiesta Filato</strong>',
+                    body: '<div class="text-center">Confermi di voler inoltrare la richiesta?</div>',
+                    footerClass: 'modal-footer justify-content-between',
+                    footer: `<button type="button" class="btn btn-default btn-flat" style="background: red; color: white;" data-dismiss="modal">Annulla</button>
+                            <button type="button" class="btn btn-primary btn-flat" onclick="(${action})()">Conferma</button>`,
+                    action: () => settingsSave('richiesta_filato', 1),
+                    onClose: () => $modal.remove(),
+                },
+                'spola': {
+                    title: '<strong>Cambio Spola</strong>',
+                    body: '<div class="text-center">Confermi di aver effettuato il cambio?</div>',
+                    footerClass: 'modal-footer justify-content-between',
+                    footer: `<button type="button" class="btn btn-default btn-flat" style="background: red; color: white;" data-dismiss="modal">Annulla</button>
+                            <button type="button" class="btn btn-primary btn-flat" onclick="(${action})()">Conferma</button>`,
+                    action: () => settingsSave('cambio_spola', 1),
+                    onClose: () => $modal.remove(),
+                },
+                'intervento': {
+                    title: '<strong>Richiesta Intervento</strong>',
+                    body: '<div class="text-center">Confermi di voler inoltrare la richiesta?</div>',
+                    footerClass: 'modal-footer justify-content-between',
+                    footer: `<button type="button" class="btn btn-default btn-flat" style="background: red; color: white;" data-dismiss="modal">Annulla</button>
+                            <button type="button" class="btn btn-primary btn-flat" onclick="(${action})()">Conferma</button>`,
+                    action: () => settingsSave('richiesta_intervento', 1),
+                    onClose: () => $modal.remove(),
+                },
+                'scansiona': {
+                    title: '<strong>Scansiona</strong>',
+                    body: '<input id="last_barcode" name="last_barcode" type="text" placeholder="Inserisci un valore..." class="form-control" data-kioskboard-type="numpad">',
+                    footerClass: 'modal-footer justify-content-between',
+                    footer: `<button type="button" class="btn btn-default btn-flat" style="background: red; color: white;" data-dismiss="modal">Annulla</button>
+                            <button type="button" class="btn btn-primary btn-flat" onclick="(${action})()">Conferma</button>`,
+                    action: () => settingsSave('last_barcode', $('#last_barcode').val()),
+                    onShow: () => {
+                        KioskBoard.run('#last_barcode');
+                        $('#last_barcode').focus();
+                    },
+                    onClose: () => $modal.remove(),
+                },
+                'modalSpola': {
+                    title: '<div class="text-center"><i class="fas fa-exclamation-triangle"></i><strong> ATTENZIONE </strong><i class="fas fa-exclamation-triangle"></i></div>',
+                    body: '<div class="text-center" style="font-size: 1.8rem;"><strong>Spola in esaurimento</strong></div>',
+                    footerClass: 'modal-footer justify-content-center bg-danger',
+                    action: () => $modal.modal('hide'),
+                    footer: `<button type="button" class="btn btn-light btn-flat" data-dismiss="modal">OK</button>`,
+                    onClose: () => $modal.remove(),
+                },
+                'arresta': {
+                    title: '<div class="text-center" style="font-weight: bold;">Spegnimento</div>',
+                    body: `<div class="text-center" style="font-size: 1.5rem;">Sicuro di voler arrestare il dispositivo?</div>
+                        <div class="d-flex justify-content-center mt-3">
+                        <button type="button" class="btn btn-default btn-flat" style="background-color: #e9ecef; color: black;" data-dismiss="modal">Annulla</button>
+                        </div>`,
+                    footerClass: 'modal-footer justify-content-between',
+                    footer: `
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" onclick="queueMessage({ action: 'reboot' });" class="btn btn-warning btn-flat" style="font-weight: bold;width: 46.5%;">Riavvia</button>
+                        <button type="button" onclick="queueMessage({ action: 'shutdown' });" class="btn btn-danger btn-flat" style="font-weight: bold;width: 46.5%;">Spegni</button>
+                        <button type="button" class="btn btn-default btn-flat" style="background-color: #e9ecef;color: black;width: 100%;" data-dismiss="modal">Annulla</button>
+                    </div>`,
+                    onClose: () => $modal.remove(),
+                }
+            };
+
+            // Merge default settings with specific modal settings
+            const finalModalSettings = { ...modalSettingsDefault, ...modalSettings[action] };
+
+            $modal.find('.modal-header').toggle(finalModalSettings.headerVisible).addClass(finalModalSettings.headerClass);
+            $modal.find('.modal-title').html(finalModalSettings.title);
+            $modal.find('.modal-dialog').addClass(finalModalSettings.positionClass);
+            $modal.find('.modal-body').addClass(finalModalSettings.bodyClass).html(finalModalSettings.body);
+            $modal.find('.modal-footer').addClass(finalModalSettings.footerClass).html(finalModalSettings.footer);
+
+            $modal.modal('show');
+
+            $modal.on('shown.bs.modal', function() {
+                if (typeof finalModalSettings.onShow === 'function') {
+                    finalModalSettings.onShow();
+                }
+            });
+
+            $modal.on('hidden.bs.modal', function() {
+                if (typeof finalModalSettings.onClose === 'function') {
+                    finalModalSettings.onClose();
+                }
+            });
+
+            forzaFocus = finalModalSettings.forzaFocus;
+        }
+</script>
--- resources/views/template_1/home.blade.php
+++ resources/views/template_1/home.blade.php
@@ -76,11 +76,11 @@
             gestisciFocus();
         });
 
-        // $badge.on('focusout', function(e) {
-        //     if (!$badge.is(":focus") && forzaFocus) {
-        //         gestisciFocus();
-        //     }
-        // });
+        $badge.on('focusout', function(e) {
+            if (!$badge.is(":focus") && forzaFocus) {
+                gestisciFocus();
+            }
+        });
 
         $(document).on('keydown', function(e) {
             if (!$badge.is(":focus") && forzaFocus) {
@@ -118,141 +118,6 @@
             keysEnterCanClose: true
         });
         KioskBoard.run('input');
-
-        var $modal;
-
-        function openModal(action) {
-            $modal = $('#modal-xl').clone();
-            $modal.removeAttr('id');
-
-            const modalContent = {
-                'filato': {
-                    title: '<strong>Richiesta Filato</strong>',
-                    body: '<div class="text-center">Confermi di voler inoltrare la richiesta?</div>',
-                    footerClass: 'modal-footer justify-content-between',
-                    action: () => settingsSave('richiesta_filato', 1),
-                    // alert_title: '<strong>Richiesta filato</strong><br>effettuata<br>con successo!',
-                    // text: '<strong>Richiesta filato</strong><br>non effettuata!',
-                },
-                'spola': {
-                    title: '<strong>Cambio Spola</strong>',
-                    body: '<div class="text-center">Confermi di aver effettuato il cambio?</div>',
-                    footerClass: 'modal-footer justify-content-between',
-                    action: () => settingsSave('cambio_spola', 1),
-                    // alert_title: '<strong>Cambio spola</strong><br>effettuato<br>con successo!',
-                    // text: '<strong>Cambio spola</strong><br>non effettuato!',
-                },
-                'intervento': {
-                    title: '<strong>Richiesta Intervento</strong>',
-                    body: '<div class="text-center">Confermi di voler inoltrare la richiesta?</div>',
-                    footerClass: 'modal-footer justify-content-between',
-                    action: () => settingsSave('richiesta_intervento', 1),
-                    // alert_title: '<strong>Richiesta intervento</strong><br>effettuata<br>con successo!',
-                    // text: '<strong>Richiesta intervento</strong><br>non effettuata!',
-                },
-                'scansiona': {
-                    title: '<strong>Scansiona</strong>',
-                    body: '<input id="last_barcode" name="last_barcode" type="text" placeholder="Inserisci un valore..." class="form-control" data-kioskboard-type="numpad">',
-                    footerClass: 'modal-footer justify-content-between',
-                    action: () => settingsSave('last_barcode', $('#last_barcode').val()),
-                    // alert_title: '<strong>Scansione</strong><br>effettuata<br>con successo!',
-                    // text: '<strong>Scansione</strong><br>non effettuata!',
-                },
-                'modalSpola': {
-                    title: '<div class="text-center"><i class="fas fa-exclamation-triangle"></i><strong> ATTENZIONE </strong><i class="fas fa-exclamation-triangle"></i></div>',
-                    body: '<div class="text-center" style="font-size: 1.8rem;"><strong>Spola in esaurimento</strong></div>',
-                    footerClass: 'modal-footer justify-content-center bg-danger',
-                    action: () => $modal.modal('hide'),
-                    // alert_title: '',
-                    // text: '',
-                },
-                'arresta': {
-                    title: '<div class="text-center" style="font-weight: bold;">Spegnimento</div>',
-                    body: `<div class="text-center" style="font-size: 1.5rem;">Sicuro di voler arrestare il dispositivo?</div>
-                           <div class="d-flex justify-content-center mt-3">
-                           <button type="button" class="btn btn-default btn-flat" style="background-color: #e9ecef; color: black;" data-dismiss="modal">Annulla</button>
-                           </div>`,
-                    footerClass: 'modal-footer justify-content-between',
-                    // alert_title: '',
-                    // text: '',
-                }
-            };
-
-            const content = modalContent[action];
-            const footer = createFooter(content.action, action);
-
-            const modalConfig = {
-                'scansiona': {
-                    modalHeaderVisible: true,
-                    modalHeaderClass: '',
-                    modalTitle: content.title,
-                    modalPositionClass: 'modal-xl',
-                    modalBodyClass: '',
-                    footerClass: content.footerClass,
-                },
-                'modalSpola': {
-                    modalHeaderVisible: true,
-                    modalHeaderClass: 'd-flex align-items-center justify-content-center bg-danger text-white',
-                    modalTitle: content.title,
-                    modalPositionClass: 'modal-fullscreen modal-dialog-centered bg-danger',
-                    modalBodyClass: 'd-flex align-items-center justify-content-center bg-danger text-white',
-                    footerClass: content.footerClass,
-                },
-                default: {
-                    modalHeaderVisible: true,
-                    modalHeaderClass: '',
-                    modalTitle: content.title,
-                    modalPositionClass: 'modal-xl modal-dialog-centered',
-                    modalBodyClass: '',
-                    footerClass: content.footerClass,
-                }
-            };
-
-            const config = modalConfig[action] || modalConfig.default;
-
-            $modal.find('.modal-header').toggle(config.modalHeaderVisible);
-            $modal.find('.modal-header').attr('class', `modal-header ${config.modalHeaderClass}`);
-            $modal.find('.modal-title').html(config.modalTitle || '');
-            $modal.find('.modal-dialog').attr('class', `modal-dialog ${config.modalPositionClass}`);
-            $modal.find('.modal-body').attr('class', `modal-body ${config.modalBodyClass}`).html(content.body);
-            $modal.find('.modal-footer').attr('class', config.footerClass).html(footer);
-
-            $modal.modal('show');
-
-            $modal.on('shown.bs.modal', function() {
-                if (action === 'scansiona') {
-                    KioskBoard.run('#last_barcode');
-                    $('#last_barcode').focus();
-                } else {
-                    gestisciFocus();
-                }
-            });
-
-            $modal.on('hidden.bs.modal', function() {
-                $modal.remove();
-            });
-        }
-
-        function createFooter(action, type) {
-            var footerButtons;
-
-            if (type === 'arresta') {
-                footerButtons =
-                    `<button type="button" class="btn btn-warning btn-flat" style="font-weight: bold;" onclick="reboot()">Riavvia</button>
-                <button type="button" class="btn btn-danger btn-flat" style="font-weight: bold;" onclick="shutdown()">Spegni</button>`;
-            } else {
-                const confirmButton = type === 'modalSpola' ?
-                    `<button type="button" class="btn btn-light btn-flat" data-dismiss="modal">OK</button>` :
-                    `<button type="button" class="btn btn-primary btn-flat" onclick="(${action})()">Conferma</button>`;
-
-                const cancelButton =
-                    '<button type="button" class="btn btn-default btn-flat" style="background: red; color: white;" data-dismiss="modal">Annulla</button>';
-
-                footerButtons = type === 'modalSpola' ? confirmButton : cancelButton + confirmButton;
-            }
-
-            return footerButtons;
-        }
 
         function settingsSave(setting, value) {
             if (!value) {
@@ -308,38 +173,6 @@
                 }
             }).fail(function(jqXHR, textStatus) {
                 console.log("Errore generico!");
-            });
-        }
-
-        function reboot() {
-            $.ajax({
-                type: 'POST',
-                url: "{{ route('reboot') }}",
-                data: {
-                    _token: '{{ csrf_token() }}'
-                }
-            }).done(function(data) {}).fail(function() {
-                Swal.fire({
-                    icon: "error",
-                    title: "Errore!",
-                    text: "Impossibile riavviare il dispositivo!"
-                });
-            });
-        }
-
-        function shutdown() {
-            $.ajax({
-                type: 'POST',
-                url: "{{ route('shutdown') }}",
-                data: {
-                    _token: '{{ csrf_token() }}'
-                }
-            }).done(function(data) {}).fail(function() {
-                Swal.fire({
-                    icon: "error",
-                    title: "Errore!",
-                    text: "Impossibile spegnere il dispositivo!"
-                });
             });
         }
     </script>
